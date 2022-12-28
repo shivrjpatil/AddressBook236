4827308e34d6fb6625d5ba7e8e739671df01e6f4
shivrjpatil <shivrajp972@gmail.com>
2022-12-28 18:29:32 +0530

    Added program for creating contact

--- AddressBook236/Program.cs
+++ AddressBook236/Program.cs
@@ -11,6 +11,24 @@ namespace AddressBook236
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to Address Book");
+            Console.WriteLine("Enter your details :-\n");
+            Console.WriteLine("First Name - " + "\n" + "Last Name - " + "\n" + "Address - " + "\n" +
+                "City Name - " + "\n" + "State Name - " + "\n" + "Zip Code - " + "\n" +
+                "Phone Number - " + "\n" + "Email ID - ");
+            Contact contact = new Contact()
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Zip = Convert.ToInt64(Console.ReadLine()),
+                PhoneNumber = Convert.ToInt64(Console.ReadLine()),
+                Email = Console.ReadLine(),
+            };
+            Console.WriteLine("\nFirst Name - " + contact.FirstName + "\nLast Name - " + contact.LastName + "\nAddress - " + contact.Address +
+                "\nCity Name - " + contact.City + "\nState Name - " + contact.State + "\nZip Code - " + contact.Zip +
+                "\nPhone Number - " + contact.PhoneNumber + "\nEmail ID - " + contact.Email);
             Console.ReadLine();
         }
     }
