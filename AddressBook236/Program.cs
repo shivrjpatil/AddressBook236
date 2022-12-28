72dcb9f085c37c8c82b3a4b4bc31c8b960a65d2f
shivrjpatil <shivrajp972@gmail.com>
2022-12-28 18:19:55 +0530

    Added welcome message

---
+++ AddressBook236/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook236
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Address Book");
+            Console.ReadLine();
+        }
+    }
+}
