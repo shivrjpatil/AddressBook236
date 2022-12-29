6666faec39b91f248703b16d63b40ebb1c0bbb43
shivrjpatil <shivrajp972@gmail.com>
2022-12-29 16:32:25 +0530

    Added Delete contact method

--- AddressBook236/Program.cs
+++ AddressBook236/Program.cs
@@ -16,23 +16,23 @@ namespace AddressBook236
                 AddressBookMain create = new AddressBookMain();
                 Console.WriteLine("Welcome to Address Book Problem Statement\n");
                 Console.WriteLine("Enter your Choice");
-                Console.WriteLine("1. Create a new Contact \n2. Edit a Contact \n3. Exit \n");
+                Console.WriteLine("1. Create a new Contact \n2. Edit a Contact \n3. Delete a Contact \n4. Exit \n");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
                     case 1:
                         Console.WriteLine("\nEnter your details :-\n");
-                        Console.WriteLine("First Name - " + "\n" + "Last Name - " + "\n" + "Address - " + "\n" +
-                                        "City Name - " + "\n" + "State Name - " + "\n" + "Zip Code - " + "\n" +
-                                        "Phone Number - " + "\n" + "Email ID - ");
                         create.Create();
                         break;
                     case 2:
                         Console.WriteLine("\nEnter your details :-\n");
-                        Console.WriteLine("First Name - " + "\n" + "Last Name - " + "\n" + "Address - " + "\n" +
-                                        "City Name - " + "\n" + "State Name - " + "\n" + "Zip Code - " + "\n" +
-                                        "Phone Number - " + "\n" + "Email ID - ");
-                        create.Edit("Lalit");
+                        create.Edit("Shiv");
+                        break;
+                    case 3:
+                        Console.WriteLine("\nEnter your details :-\n");
+
+                        Console.WriteLine("\nEnter name whose detail you want to delete :-\n");
+                        create.Delete("shiv");
                         break;
                     default:
                         Console.WriteLine("Try Again");
@@ -41,5 +41,6 @@ namespace AddressBook236
                 }
             }
         }
+        
     }
 }
